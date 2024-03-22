7a8e9abfcb763ccb2bfcd376d745ec9a2c7f5b39
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-22 23:03:16 +0330

    add rtl plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Button, createTheme, ThemeProvider} from "@mui/material";
+import {CacheProvider} from '@emotion/react';
+import createCache from '@emotion/cache';
+import {prefixer} from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
+import {Helmet, HelmetProvider} from "react-helmet-async";
+
+const theme = createTheme({
+    direction: 'rtl',
+});
+
+// Create rtl cache
+const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+});
+
+function Rtl(props) {
+    return <CacheProvider value={cacheRtl}>{props.children}</CacheProvider>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+                <HelmetProvider>
+                    <Helmet>
+                        <title>test</title>
+                    </Helmet>
+                    <Button variant={'contained'}>test</Button>
+                </HelmetProvider>
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
 
 export default App
