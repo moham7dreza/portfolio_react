16c050cb31914c79017e4cf94c37311771a6fdd4
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-25 00:02:24 +0330

    work with hidden components

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,5 +1,5 @@
 import Grid2 from "@mui/material/Unstable_Grid2";
-import {Avatar, Box, Divider, Typography} from "@mui/material";
+import {Avatar, Box, Divider, Hidden, Typography} from "@mui/material";
 import avatar from "../../assets/avatar.png";
 import {grey} from "@mui/material/colors";
 
@@ -9,7 +9,10 @@ export const Sidebar = () => {
             {/*sidebar*/}
             <Grid2 xs={0} sm={0} md={3} lg={3} xl={3} sx={{backgroundColor: grey[800]}}>
                 <Box sx={{justifyContent: 'center', textAlign: 'center', mt: 2}}>
-                    <Avatar src={avatar} sx={{height: 200, width: 200, m: '0 auto'}}/>
+                    {/*can be dynamically down ex according to state*/}
+                    <Hidden mdDown={true}>
+                        <Avatar src={avatar} sx={{height: 200, width: 200, m: '0 auto'}}/>
+                    </Hidden>
                     <Typography variant='h6' color='whitesmoke'>sidebar</Typography>
                     <Typography variant='caption' color='whitesmoke'>sidebar</Typography>
                     <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
