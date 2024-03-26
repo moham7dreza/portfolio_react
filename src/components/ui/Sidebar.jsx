23e3c4e9172dda4ec3d75c431d4d2d36c5fd3ec9
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-27 01:23:08 +0330

    add tabs to sidebar

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -1,7 +1,17 @@
 import Grid from "@mui/material/Unstable_Grid2";
-import {Avatar, Box, Divider, Hidden, Typography} from "@mui/material";
+import {Avatar, Box, Divider, Hidden, Tab, Tabs, Typography} from "@mui/material";
 import avatar from "../../assets/avatar.png";
 import {grey} from "@mui/material/colors";
+import {
+    ConnectWithoutContactRounded,
+    CopyrightRounded,
+    FaceRounded,
+    FavoriteRounded,
+    HomeRounded,
+    MessageRounded,
+    TerminalRounded,
+    TextSnippetRounded
+} from "@mui/icons-material";
 
 export const Sidebar = () => {
     return (
@@ -16,6 +26,30 @@ export const Sidebar = () => {
                     <Typography variant='h6' color='whitesmoke'>sidebar</Typography>
                     <Typography variant='caption' color='whitesmoke'>sidebar</Typography>
                     <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
+                    <Tabs orientation='vertical' variant={'scrollable'} scrollButtons={'auto'} allowScrollButtonsMobile>
+                        <Tab label={'home page'} icon={<HomeRounded/>} iconPosition={'start'}/>
+                        <Tab label={'about me'} icon={<FaceRounded/>} iconPosition={'start'}/>
+                        <Tab label={'my resume'} icon={<TextSnippetRounded/>} iconPosition={'start'}/>
+                        <Tab label={'my projects'} icon={<TerminalRounded/>} iconPosition={'start'}/>
+                        <Tab label={'comments'} icon={<MessageRounded/>} iconPosition={'start'}/>
+                        <Tab label={'contact'} icon={<ConnectWithoutContactRounded/>} iconPosition={'start'}/>
+                    </Tabs>
+                    <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
+                    <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'end', height: 100}}>
+                        <Typography variant='subtitle2' color={'whitesmoke'}
+                                    sx={{display: 'flex', alignItems: 'center', flexDirection: 'row-reverse', gap: 1}}>
+                            designed
+                            <FavoriteRounded sx={{color: 'tomato', height: 20}}/>
+                        </Typography>
+                    </Box>
+                    <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
+                    <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'end', height: 100}}>
+                        <Typography variant='subtitle2' color={'whitesmoke'}
+                                    sx={{display: 'flex', alignItems: 'center', flexDirection: 'row-reverse', gap: 1}}>
+                            copyright 2024
+                            <CopyrightRounded sx={{color: 'green', height: 20}}/>
+                        </Typography>
+                    </Box>
                 </Box>
             </Grid>
         </>
