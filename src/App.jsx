16773f7895104c7d43d64988f01dea42941626a9
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-28 00:11:03 +0330

    implement tabs functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {MainLayout} from "./components/layouts/MainLayout.jsx";
 import {Sidebar} from "./components/ui/Sidebar.jsx";
 import {ContentContainer} from "./components/ui/ContentContainer.jsx";
 import {useState} from "react";
+import {TabPanel} from "./components/tabs/TabPanel.jsx";
+import {Typography} from "@mui/material";
 
 function App() {
     const [value, setValue] = useState(0)
@@ -15,7 +17,38 @@ function App() {
             {/*according to specific route we can render specific layout*/}
             <MainLayout>
                 <Sidebar value={value} handleChange={handleChange}/>
-                <ContentContainer/>
+                <ContentContainer>
+                    <TabPanel value={value} index={0}>
+                        <Typography>
+                            home page
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <Typography>
+                            about me
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={2}>
+                        <Typography>
+                            my resume
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={3}>
+                        <Typography>
+                            my projects
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={4}>
+                        <Typography>
+                            comments
+                        </Typography>
+                    </TabPanel>
+                    <TabPanel value={value} index={5}>
+                        <Typography>
+                            contact
+                        </Typography>
+                    </TabPanel>
+                </ContentContainer>
             </MainLayout>
         </>
     )
