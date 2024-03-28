e4d58abbb704c70ad10b6d237d61ae399fff8cdb
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-29 00:51:38 +0330

    use drawer component and define state for it

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,67 +1,33 @@
 import Grid from "@mui/material/Unstable_Grid2";
-import {Avatar, Box, Divider, Hidden, Tab, Tabs, Typography} from "@mui/material";
-import avatar from "../assets/avatar.png";
+import {Drawer} from "@mui/material";
 import {grey} from "@mui/material/colors";
-import {
-    ConnectWithoutContactRounded,
-    CopyrightRounded,
-    FaceRounded,
-    FavoriteRounded,
-    HomeRounded,
-    MessageRounded,
-    TerminalRounded,
-    TextSnippetRounded
-} from "@mui/icons-material";
+import {useState} from "react";
+import {DrawerContent} from "./ui/DrawerContent.jsx";
 
 export const Sidebar = ({value, handleChange}) => {
-    // we setup unique id for each tab
-    const tabProps = (index) => {
-        return {
-            id: `sidebar-tab-${index}`,
-            "aria-controls": `tab-panel-${index}`,
-        }
+    const [drawerOpen, setDrawerOpen] = useState(false)
+
+    const handleDrawerOpen = () => {
+        setDrawerOpen(!drawerOpen)
     }
+
     return (
         <>
             {/*sidebar*/}
             <Grid xs={0} sm={0} md={3} lg={3} xl={3} sx={{backgroundColor: grey[800]}}>
-                <Box sx={{justifyContent: 'center', textAlign: 'center', mt: 2}}>
-                    {/*can be dynamically down ex according to state*/}
-                    <Hidden mdDown={true}>
-                        <Avatar alt='avatar' src={avatar} sx={{height: 200, width: 200, m: '0 auto'}}/>
-                    </Hidden>
-                    <Typography variant='h6' color='whitesmoke'>sidebar</Typography>
-                    <Typography variant='caption' color='whitesmoke'>sidebar</Typography>
-                    <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
-                    {/*parent tab gets value and handle change*/}
-                    <Tabs value={value} onChange={handleChange} orientation='vertical' variant={'scrollable'}
-                          scrollButtons={'auto'} allowScrollButtonsMobile>
-                        {/*each tab has unique index*/}
-                        <Tab {...tabProps(0)} label={'home page'} icon={<HomeRounded/>} iconPosition={'start'}/>
-                        <Tab {...tabProps(1)} label={'about me'} icon={<FaceRounded/>} iconPosition={'start'}/>
-                        <Tab {...tabProps(2)} label={'my resume'} icon={<TextSnippetRounded/>} iconPosition={'start'}/>
-                        <Tab {...tabProps(3)} label={'my projects'} icon={<TerminalRounded/>} iconPosition={'start'}/>
-                        <Tab {...tabProps(4)} label={'comments'} icon={<MessageRounded/>} iconPosition={'start'}/>
-                        <Tab {...tabProps(5)} label={'contact'} icon={<ConnectWithoutContactRounded/>}
-                             iconPosition={'start'}/>
-                    </Tabs>
-                    <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
-                    <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'end', height: 100}}>
-                        <Typography variant='subtitle2' color={'whitesmoke'}
-                                    sx={{display: 'flex', alignItems: 'center', flexDirection: 'row-reverse', gap: 1}}>
-                            designed
-                            <FavoriteRounded sx={{color: 'tomato', height: 20}}/>
-                        </Typography>
-                    </Box>
-                    <Divider variant='middle' sx={{mt: 2}} color={grey[900]}/>
-                    <Box sx={{flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'end', height: 100}}>
-                        <Typography variant='subtitle2' color={'whitesmoke'}
-                                    sx={{display: 'flex', alignItems: 'center', flexDirection: 'row-reverse', gap: 1}}>
-                            copyright 2024
-                            <CopyrightRounded sx={{color: 'green', height: 20}}/>
-                        </Typography>
-                    </Box>
-                </Box>
+                {/*sidebar content which follow grid system breakpoints and hidden below md*/}
+                <DrawerContent value={value} handleChange={handleChange}/>
+                {/*if we use drawer to toggle sidebar it has not inherited of grid system width because of grid have many breakpoints*/}
+                {/*drawer has not compatible with grid system width*/}
+                {/*drawer have been created a new layer in layout seperated from grid system*/}
+                {/*so we can not give width to drawer dynamically according to grid breakpoints*/}
+                {/*use drawer and show it below md*/}
+                {/*use sx with custom css to modify drawer width*/}
+                {/*use temporary variant to get shadow in content section which means drawer can be hidden*/}
+                <Drawer sx={{"& .MuiDrawer-paper": {width: 320}}} open={drawerOpen} variant={'temporary'}
+                        onClose={() => setDrawerOpen(false)}>
+                    <DrawerContent value={value} handleChange={handleChange}/>
+                </Drawer>
             </Grid>
         </>
     )
