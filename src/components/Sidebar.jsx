da5f22dfd612ab4866e046c2c04ca12274485664
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-03-29 11:18:53 +0330

    add floating action button for show below md

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,9 @@
 import Grid from "@mui/material/Unstable_Grid2";
-import {Drawer} from "@mui/material";
-import {grey} from "@mui/material/colors";
+import {Box, Drawer, Fab} from "@mui/material";
+import {grey, red} from "@mui/material/colors";
 import {useState} from "react";
 import {DrawerContent} from "./ui/DrawerContent.jsx";
+import {MenuRounded} from "@mui/icons-material";
 
 export const Sidebar = ({value, handleChange}) => {
     const [drawerOpen, setDrawerOpen] = useState(false)
@@ -15,6 +16,14 @@ export const Sidebar = ({value, handleChange}) => {
         <>
             {/*sidebar*/}
             <Grid xs={0} sm={0} md={3} lg={3} xl={3} sx={{backgroundColor: grey[800]}}>
+                {/*hidden box below md*/}
+                <Box sx={{display: {md: 'none'}}}>
+                    <Fab aria-label={'floating action button'} size='small' sx={{m: 2, backgroundColor: red[500]}}
+                         onClick={handleDrawerOpen}>
+                        <MenuRounded/>
+                    </Fab>
+                </Box>
+
                 {/*sidebar content which follow grid system breakpoints and hidden below md*/}
                 <DrawerContent value={value} handleChange={handleChange}/>
                 {/*if we use drawer to toggle sidebar it has not inherited of grid system width because of grid have many breakpoints*/}
