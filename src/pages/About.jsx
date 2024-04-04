3f48f2eca009cd81fc3c59e9cb0d476b7967e3c2
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-04-04 13:07:42 +0330

    add dev skills with progress to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,9 @@
 import {Avatar, Card, CardContent, Chip, Divider, Typography} from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import {CodeRounded} from "@mui/icons-material";
+import {CodeRounded, Html, SelfImprovementRounded} from "@mui/icons-material";
 import avatar from "../assets/images/avatar.png";
 import {Info} from "./components/panel/Info.jsx";
+import {Skill} from "./components/panel/Skill.jsx";
 
 const About = () => {
     return (
@@ -35,6 +36,19 @@ const About = () => {
                                         m: '0 auto',
                                         display: {xs: 'none', sm: 'none', md: 'block'}
                                     }}/>
+                        </Grid>
+                    </Grid>
+                    <Grid container>
+                        <Grid sx={{width: 1, mt: 1}}>
+                            <Divider textAlign='center' sx={{'&::before, &::after': {borderColor: 'white'}}}>
+                                <Chip color='secondary' label={
+                                    <Typography variant='body1' color='black' sx={{textAlign: 'center'}}>
+                                        i am a backend developer
+                                    </Typography>
+                                } sx={{p: 3}} icon={<SelfImprovementRounded/>}></Chip>
+                            </Divider>
+                            <Skill label='Html' icon={<Html/>} percent={90} color={'primary'}/>
+                            <Skill label='Js' icon={<Html/>} percent={80} color={'secondary'}/>
                         </Grid>
                     </Grid>
                 </CardContent>
---
+++ src/pages/components/panel/Skill.jsx
@@ -0,0 +1,24 @@
+import {Box, Chip, Divider, LinearProgress, Typography} from "@mui/material";
+
+export const Skill = ({label, icon, percent, color}) => {
+    return (
+        <>
+            <Divider textAlign='right'>
+                <Chip icon={<Box component='img' src={icon} sx={{height: 30}}/>} color={color}
+                      sm={{color: 'black', p: 3}}
+                      label={label}/>
+            </Divider>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Box sx={{minWidth: 35}}>
+                    <Typography variant='body2' color={color}>
+                        {percent} %
+                    </Typography>
+                </Box>
+                <Box sx={{width: 1, mr: 1}}>
+                    <LinearProgress sx={{borderRadius: 2, height: 10}} variant='determinate' value={percent}
+                                    color={color}/>
+                </Box>
+            </Box>
+        </>
+    )
+}
