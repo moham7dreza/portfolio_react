213f74fed43d910284ecf766cebb0547fda8fd85
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-04-12 21:49:18 +0330

    fix Home.jsx page center content bg and text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import MainImage from "../assets/images/Portfolio-Desk.jpg";
-import {Box, Typography} from "@mui/material";
+import {Box, Typography, useTheme} from "@mui/material";
 import {useCallback, useEffect, useRef, useState} from "react";
 import Typed from "typed.js";
 import {loadFull} from "tsparticles";
@@ -7,8 +7,11 @@ import Particles from "react-particles";
 import {mouseAttraction} from "../constants/panel/particles";
 import TextTransition, {presets} from "react-text-transition";
 import {Helmet} from "react-helmet-async";
+import {grey} from "@mui/material/colors";
 
 const Home = ({helmetTitle}) => {
+    const mode = useTheme().palette.mode
+
     // we need this state for handle strings index
     const [index, setIndex] = useState(0)
 
@@ -57,38 +60,48 @@ const Home = ({helmetTitle}) => {
             gap: 2
         }}>
             <Particles id='tsparticles' init={particlesInit} loaded={particlesLoaded} options={mouseAttraction}/>
-            <Box component='div' sx={{display: 'flex'}}>
-                <Typography variant='h3' color={'tomato'}>
-                    {"{{"}
-                </Typography>
-                <Typography ref={nameElement} variant='h3' color={'aqua'}></Typography>
-                <Typography variant='h3' color={'tomato'}>
-                    {"}}"}
-                </Typography>
-            </Box>
-            <Box component='div' sx={{display: 'flex', alignItems: 'center'}}>
-                {/*wobbly -> effect after string shown*/}
-                <TextTransition springConfig={presets.wobbly}>
-                    <Typography sx={{mt: 2, textDecoration: 'underline', textDecorationColor: 'blue'}}
-                                variant='h4' color={'aqua'}>
-                        {/*we need to make loop for show 1 one strings every 3 seconds*/}
-                        {/*index = 0 => strings[0 % 4 = 0]*/}
-                        {/*index = 1 => strings[1 % 4 = 1]*/}
-                        {/*index = 2 => strings[2 % 4 = 2]*/}
-                        {/*index = 3 => strings[3 % 4 = 3]*/}
-                        {/*index = 4 => strings[4 % 4 = 0]*/}
-                        {/*index = 5 => strings[5 % 4 = 1]*/}
-                        {/*index = 6 => strings[6 % 4 = 2]*/}
-                        {/*index = 7 => strings[7 % 4 = 3]*/}
-                        {/*index = 8 => strings[8 % 4 = 0]*/}
-                        {/*...*/}
-                        {/*as so see with repeatedly we can indexes of 0, 1, 2, 3 every 3 seconds*/}
-                        {strings[index % strings.length]}
+            <Box component='div' bgcolor={mode === 'dark' ? grey[800] : grey[500]} sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: 2,
+                p: 4,
+                borderRadius: 5
+            }}>
+                <Box component='div' sx={{display: 'flex'}}>
+                    <Typography variant='h3' color={'tomato'}>
+                        {"{{"}
                     </Typography>
-                </TextTransition>
-                <Typography variant='h4' color='whitesmoke' sx={{mr: 1, mt: 2}}>
-                    i am
-                </Typography>
+                    <Typography ref={nameElement} variant='h3' color={'text.primary'}></Typography>
+                    <Typography variant='h3' color={'tomato'}>
+                        {"}}"}
+                    </Typography>
+                </Box>
+                <Box component='div' sx={{display: 'flex', alignItems: 'center'}}>
+                    {/*wobbly -> effect after string shown*/}
+                    <TextTransition springConfig={presets.wobbly}>
+                        <Typography sx={{mt: 2, textDecoration: 'underline', textDecorationColor: 'blue'}}
+                                    variant='h4' color={'text.secondary'}>
+                            {/*we need to make loop for show 1 one strings every 3 seconds*/}
+                            {/*index = 0 => strings[0 % 4 = 0]*/}
+                            {/*index = 1 => strings[1 % 4 = 1]*/}
+                            {/*index = 2 => strings[2 % 4 = 2]*/}
+                            {/*index = 3 => strings[3 % 4 = 3]*/}
+                            {/*index = 4 => strings[4 % 4 = 0]*/}
+                            {/*index = 5 => strings[5 % 4 = 1]*/}
+                            {/*index = 6 => strings[6 % 4 = 2]*/}
+                            {/*index = 7 => strings[7 % 4 = 3]*/}
+                            {/*index = 8 => strings[8 % 4 = 0]*/}
+                            {/*...*/}
+                            {/*as so see with repeatedly we can indexes of 0, 1, 2, 3 every 3 seconds*/}
+                            {strings[index % strings.length]}
+                        </Typography>
+                    </TextTransition>
+                    <Typography variant='h4' color='whitesmoke' sx={{mr: 1, mt: 2}}>
+                        i am
+                    </Typography>
+                </Box>
             </Box>
         </Box>
     </>)
