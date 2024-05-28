757b7d99f9b4068271a49f7c8ef66b5f528a098c
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2024-05-29 00:19:08 +0330

    add layout and navbar and install preline ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,24 @@
 function App() {
 
   return (
-    <>
-
-    </>
+      <>
+        <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+          <h1 className="block text-2xl font-bold text-white sm:text-4xl">Cover Page</h1>
+          <p className="mt-3 text-lg text-gray-300 dark:text-neutral-400">Cover is a one-page template for
+            building simple and beautiful home pages using Tailwind CSS.</p>
+          <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+            <a className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-white text-gray-800 hover:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none"
+               target="parent" href="">
+              <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
+                   height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                   strokeLinecap="round" strokeLinejoin="round">
+                <path d="m15 18-6-6 6-6"/>
+              </svg>
+              Back to examples
+            </a>
+          </div>
+        </div>
+      </>
   )
 }
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,9 +3,21 @@ import {Navbar} from "../components/Navbar.jsx";
 
 export const MainLayout = () => {
     return (
-        <>
+        <div className="max-w-full h-screen flex flex-col mx-auto size-full bg-gray-800">
             <Navbar/>
-            <Outlet/>
-        </>
+
+            <main id="content">
+                <Outlet/>
+            </main>
+
+            <footer className="mt-auto text-center py-5">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <p className="text-sm text-gray-400 dark:text-neutral-400">Cover template for <a
+                        className="text-white decoration-2 underline underline-offset-2 font-medium hover:text-gray-200 hover:decoration-gray-400"
+                        href="">Preline</a></p>
+                </div>
+            </footer>
+
+        </div>
     )
 }
