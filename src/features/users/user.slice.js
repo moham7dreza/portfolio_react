740db369de39dc94ceb6d80dbf05197f74c96a50
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2024-06-04 16:40:06 +0330

    add crud thunks for user

--- src/features/users/user.slice.js
+++ src/features/users/user.slice.js
@@ -32,3 +32,27 @@ export const fetchUsers = createAsyncThunk(
         return response.data;
     }
 )
+
+export const createUser = createAsyncThunk(
+    "users/createUser",
+    async (user) => {
+        const response = await UserService.store(user);
+        return response.data;
+    }
+)
+
+export const updateUser = createAsyncThunk(
+    "users/updateUser",
+    async (user) => {
+        const response = await UserService.update(user, user.id);
+        return response.data;
+    }
+)
+
+export const deleteUser = createAsyncThunk(
+    "users/deleteUser",
+    async (id) => {
+        await UserService.destroy(id);
+        return id;
+    }
+)
