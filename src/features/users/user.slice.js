323b38f1e27d577f77a0169d06676580d3ecbabd
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-06-06 11:50:10 +0330

    refactor user.slice.js to use entity adapter

--- src/features/users/user.slice.js
+++ src/features/users/user.slice.js
@@ -1,9 +1,13 @@
-import {createAsyncThunk, createSlice, nanoid} from "@reduxjs/toolkit";
+import {createAsyncThunk, createEntityAdapter, createSlice, nanoid} from "@reduxjs/toolkit";
 import * as UserService from "../../services/UserService.js";
+
+const userAdapter = createEntityAdapter()
+
+const initialState = userAdapter.getInitialState()
 
 const usersSlice = createSlice({
     name: 'users',
-    initialState: [],
+    initialState,
     reducers: {
         addUser: (state, action) => {
             state.push({
@@ -13,19 +17,30 @@ const usersSlice = createSlice({
         }
     },
     extraReducers: builder => {
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload;//immer js will replace this with current state value
-        }).addCase(createUser.fulfilled, (state, action) => {
-            state.push(action.payload)
-        }).addCase(deleteUser.fulfilled, (state, action) => {
-            return state.filter(user => user.id !== action.payload)
-        })
+        // builder.addCase(fetchUsers.fulfilled, (state, action) => {
+        //     return action.payload;//immer js will replace this with current state value
+        // }).addCase(createUser.fulfilled, (state, action) => {
+        //     state.push(action.payload)
+        // }).addCase(deleteUser.fulfilled, (state, action) => {
+        //     return state.filter(user => user.id !== action.payload)
+        // })
+
+        // replace new values with current state -> setAll
+        // otherwise we can use upsertMany
+        builder.addCase(fetchUsers.fulfilled, userAdapter.setAll)
+            .addCase(createUser.fulfilled, userAdapter.addOne)
+            .addCase(deleteUser.fulfilled, userAdapter.removeOne)
     }
 })
 
-export const selectUsers = state => state.users;
+// export const selectUsers = state => state.users;
+//
+// export const selectById = (state, userId) => state.users.find(user => user.id === userId)
 
-export const selectById = (state, userId) => state.users.find(user => user.id === userId)
+export const {
+    selectAll: selectUsers,
+    selectById
+} = userAdapter.getSelectors(state => state.users)
 
 export default usersSlice.reducer;
 
