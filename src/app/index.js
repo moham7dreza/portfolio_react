7186a0619c5db24b313e3c229dd3bb85fcdef5f7
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-06-06 13:15:01 +0330

    modify store to work with RTK Query

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import blogsReducer from "../features/blogs/blog.slice.js";
 import usersReducer, {fetchUsers} from "../features/users/user.slice.js";
+import {apiSlice} from "../api/api.slice.js";
 
 export const store = configureStore({
     reducer: {
         blogs: blogsReducer,
         users: usersReducer,
-    }
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    // add apiSlice middleware to default middlewares list
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
 })
 
 store.dispatch(fetchUsers())
